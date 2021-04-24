2a2e07247caffb13f2851f2a36184d4c4f6e1dd9
DimovDimo <d.dimov1996@gmail.com>
2021-04-24 08:17:34 +0300

    added image in canvas

--- app/resources/js/change-image-colors.js
+++ app/resources/js/change-image-colors.js
@@ -1,2 +1,10 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
+
+let image = new Image();
+let imagePath = 'resources/images/parrot.jpg';
+image.src = imagePath;
+
+image.onload = function () {
+    context.drawImage(image, 0, 0);
+};
