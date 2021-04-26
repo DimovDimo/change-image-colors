618dbb937c878c8dd394f9fbf3bd54f068033699
DimovDimo <d.dimov1996@gmail.com>
2021-04-26 11:00:17 +0300

    added function invert

--- app/resources/js/change-image-colors.js
+++ app/resources/js/change-image-colors.js
@@ -11,4 +11,26 @@ image.onload = function () {
 
 function original() {
     context.drawImage(image, 0, 0);
-};
+}
+
+function invert() {
+    //original()
+    context.drawImage(img, 0, 0);
+
+    //get data
+    let imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+
+    for (let i = 0; i < data.length; i = i + 4) {
+        let red = data[i];
+        let green = data[i + 1];
+        let blue = data[i + 2];
+
+        red = 255 - red;
+        green = 255 - green;
+        blue = 255 - blue;
+    }
+
+    //put data
+    context.putImageData(imgData, 0, 0);
+}
